123cbdebd9f3e44adb519197dfe656fd0bdb7a2a
Sebastian <sebasgiancaspro@gmail.com>
2021-06-30 10:44:37 -0300

    Creacion de uso de OpenLayers con Angular

---
+++ src/app/ol/ol.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+@Component({
+  selector: 'app-ol',
+  templateUrl: './ol.component.html',
+  styleUrls: ['./ol.component.css']
+})
+export class OlComponent implements OnInit {
+
+  map: Map | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.map = new Map({
+      view: new View({
+        center: [0, 0],
+        zoom: 1,
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'ol-map'
+    });
+  }
+
+}
